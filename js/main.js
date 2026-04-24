4764b0eae671555d84a9e432840ff41e3cced4b2
alexanderbertoni9-sudo <alexander.bertoni9@gmail.com>
2026-04-24 12:13:02 -0400

    Feat: Added a boba image library

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
 function testDisplay(){
     alert("Hello World")
 }
+
+const button = document.getElementById("addButton");
+const counterText = document.getElementById("counter")
+
+let count = 0;
+
+button.addEventListener('click', () => {
+  count++; // Increment the number
+  counterText.textContent = count; // Update the display
+});
