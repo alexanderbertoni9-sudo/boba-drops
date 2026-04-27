077f296b5155a3a4917fd3b17bf724537f862c55
alexanderbertoni9-sudo <alexander.bertoni9@gmail.com>
2026-04-26 21:12:33 -0400

    feat: Added boba that replaces mouse cursor

--- js/main.js
+++ js/main.js
@@ -1,13 +1,18 @@
-function testDisplay(){
-    alert("Hello World")
-}
+const img = document.getElementById("cursor-follower");
+
+document.addEventListener("mousemove", function (e) {
+  img.style.left = e.clientX + "px";
+  img.style.top = e.clientY + "px";
+});
 
 const button = document.getElementById("addButton");
-const counterText = document.getElementById("counter")
+const counterText = document.getElementById("counter");
 
 let count = 0;
 
-button.addEventListener('click', () => {
-  count++; // Increment the number
-  counterText.textContent = count; // Update the display
-});
+if (button && counterText) {
+  button.addEventListener('click', () => {
+    count++;
+    counterText.textContent = count;
+  });
+}
